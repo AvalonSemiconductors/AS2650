7b4b000126e7177a8656d3dc140512d9c57849c1
Tholin <cutie@tholin.dev>
2023-11-28 13:23:25 +0100

    Make logo larger

--- PlacementPreview.java
+++ PlacementPreview.java
@@ -23,10 +23,10 @@ public class PlacementPreview {
 		
 		Macro[] macros = new Macro[] {
 			new Macro(300, 1450, 1050, 700, Color.GREEN, "AS2650"), //wrapped_as2650
-			new Macro(300, 2450, 300, 352, new Color(240, 128, 32), "Avali Logo"), //avali_logo
+			new Macro(300, 2350, 375, 440, new Color(240, 128, 32), "Avali Logo"), //avali_logo
 			new Macro(2400, 1900, 250, 250, Color.RED, "GPIOs"), //gpios
 			new Macro(2350, 2250, 425, 425, Color.BLUE, "Timers & PWM"), //timers
-			new Macro(800, 2500, 275, 275, Color.ORANGE, "Serial Ports"), //serial_ports
+			new Macro(1000, 2500, 275, 275, Color.ORANGE, "Serial Ports"), //serial_ports
 			new Macro(2400, 1500, 210, 210, new Color(128, 64, 255), "Boot ROM"), //boot_rom
 			new Macro(1500, 2000, 750, 750, new Color(245/*244*/, 130, 133), "SID"), //sid_top
 			new Macro(300, 800, 1100, 250, new Color(100, 200, 178), "RAM Controller"), //ram_controller
